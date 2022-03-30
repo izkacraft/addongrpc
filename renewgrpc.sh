14a3ad1e1d14600aece00be774e84a27b61e19ae
Izhan Jafry <97201444+izhanworks@users.noreply.github.com>
2022-03-31 02:12:37 +0800

    Update renewgrpc.sh

--- renewgrpc.sh
+++ renewgrpc.sh
@@ -18,7 +18,7 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
 clear
-NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/rare/xray/conf/vmessgrpc.json")
+NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/rare/xray/grpc/vmessgrpc.json")
 	if [[ ${NUMBER_OF_CLIENTS} == '0' ]]; then
 		clear
 		echo ""
@@ -31,7 +31,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/rare/xray/conf/vmessgrpc.json")
 	echo "Select the existing client you want to renew"
 	echo " Press CTRL+C to return"
 	echo -e "==============================="
-	grep -E "^### " "/etc/rare/xray/conf/vmessgrpc.json" | cut -d ' ' -f 2-3 | nl -s ') '
+	grep -E "^### " "/etc/rare/xray/grpc/vmessgrpc.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
@@ -40,16 +40,16 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/rare/xray/conf/vmessgrpc.json")
 		fi
 	done
 read -p "Expired (Days): " masaaktif
-user=$(grep -E "^### " "/etc/rare/xray/conf/vmessgrpc.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
-exp=$(grep -E "^### " "/etc/rare/xray/conf/vmessgrpc.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
+user=$(grep -E "^### " "/etc/rare/xray/grpc/vmessgrpc.json" | cut -d ' ' -f 2 | sed -n "${CLIENT_NUMBER}"p)
+exp=$(grep -E "^### " "/etc/rare/xray/grpc/vmessgrpc.json" | cut -d ' ' -f 3 | sed -n "${CLIENT_NUMBER}"p)
 now=$(date +%Y-%m-%d)
 d1=$(date -d "$exp" +%s)
 d2=$(date -d "$now" +%s)
 exp2=$(( (d1 - d2) / 86400 ))
 exp3=$(($exp2 + $masaaktif))
 exp4=`date -d "$exp3 days" +"%Y-%m-%d"`
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/rare/xray/conf/vmessgrpc.json
-sed -i "s/### $user $exp/### $user $exp4/g" /etc/rare/xray/conf/vlessgrpc.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/rare/xray/grpc/vmessgrpc.json
+sed -i "s/### $user $exp/### $user $exp4/g" /etc/rare/xray/grpc/vlessgrpc.json
 systemctl restart xray.service
 service cron restart
 clear
